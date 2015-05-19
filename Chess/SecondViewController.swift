c2a04e744696dea92956993854a2826268a8ef09
kzeng10 <kzeng10@gmail.com>
2015-05-19 10:13:39 -0700

    initial commit

---
+++ Chess/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Chess
+//
+//  Created by Kevin Zeng on 5/19/15.
+//  Copyright (c) 2015 Kevin Zeng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
